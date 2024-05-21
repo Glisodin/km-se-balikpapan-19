dc54d989866dfaf0d49bbea19bcfb435894faed2
Glisodin <bagusaji6@gmail.com>
2024-05-21 21:46:09 +0700

    mengubah typo & memindahkan/menghapus folder image ke img

---
+++ js/ji24.js
@@ -0,0 +1,44 @@
+const ctx = document.getElementById("myChart");
+
+let myChart;
+let jsonData;
+
+fetch("final_dataset.json")
+  .then(function (response) {
+    if (response.ok == true) {
+      return response.json();
+    }
+  })
+  .then(function (data) {
+    jsonData = data;
+    createchart(data, "bar");
+  });
+
+function setchartType(chartType) {
+  myChart.destroy();
+  createchart(jsonData, chartType);
+}
+
+function createchart(data, type) {
+  myChart = new Chart(ctx, {
+    type: type,
+    data: {
+      labels: data.map((row) => row.product_category),
+      datasets: [
+        {
+          label: "Top 5 Product Category",
+          data: data.map((row) => row.store_location),
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+      maintainAspectRatio: false,
+    },
+  });
+}
